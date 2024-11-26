70fe74fab9698b78d27116723a30be82a0564715
narapark7561 <narapark7561@gmail.com>
2024-11-26 00:48:22 -0700

    implementing contact form

--- portfolio-react/src/Components/Contact/Contact.jsx
+++ portfolio-react/src/Components/Contact/Contact.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Contact.css'
 import theme_pattern from '../../assets/custom_theme_removed.png'
+import mail_icon from '../../assets/mail_icon.svg'
+import location_icon from '../../assets/location_icon.svg'
+
+
 
 
 const Contact = () => {
@@ -14,11 +18,22 @@ const Contact = () => {
           <div className="contact-left">
             <h1>Let's connect</h1>
             <div className="contact-details">
-                <div className="contact-detail"></div>
-                <div className="contact-detail"></div>
-                <div className="contact-detail"></div>
+                <div className="contact-detail">
+                    <img src={mail_icon} alt=""/><p>narapark7561@gmail.com</p>
+                </div>
+                <div className="contact-detail">
+                <img src={location_icon} alt=""/><p>Calgary, Canada</p>
+                </div>
             </div>
           </div>
+          <form className="contact-right">
+            <label>Your Name</label>
+            <input type="text" placeholder='Enter your name' name='name'></input>
+            <label>Your Email</label>
+            <input type="email" placeholder='Enter your email' name='email'></input>
+            <label>Write your message here</label>
+            <input type="email" placeholder='Enter your email' name='email'></input>
+          </form>
         </div>
       </div>
     );
