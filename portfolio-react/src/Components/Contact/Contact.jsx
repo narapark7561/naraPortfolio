44a6eeae103d0dfa59840db5fe5d6b4420385ab0
narapark7561 <narapark7561@gmail.com>
2024-11-26 00:38:29 -0700

    working on contact info

--- portfolio-react/src/Components/Contact/Contact.jsx
+++ portfolio-react/src/Components/Contact/Contact.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import './Contact.css'
+import theme_pattern from '../../assets/custom_theme_removed.png'
+
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+    return (
+      <div className="contact">
+        <div className="contact-title">
+          <h1>Get in touch</h1>
+          <img src={theme_pattern} alt="" />
+        </div>
+        <div className="contact-section">
+          <div className="contact-left">
+            <h1>Let's connect</h1>
+            <div className="contact-details">
+                <div className="contact-detail"></div>
+                <div className="contact-detail"></div>
+                <div className="contact-detail"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
 
 export default Contact
