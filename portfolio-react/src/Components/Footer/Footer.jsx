a84edbd7c57917691842bf95b1efdd75da5a5a07
narapark7561 <narapark7561@gmail.com>
2024-11-26 16:48:09 -0700

    Completed footer section with css

--- portfolio-react/src/Components/Footer/Footer.jsx
+++ portfolio-react/src/Components/Footer/Footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './Footer.css'
+import nara from '../../assets/nara_final_logo.png'
+import user_icon from '../../assets/user_icon.svg'
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="footer-top">
+        <div className="footer-top-left">
+          <img src={nara} alt=""/>
+          <p>
+          I am a passionate Full Stack Developer with hands-on experience, dedicated to creating efficient, user-friendly solutions that bridge technology and people.</p>
+        </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">© 2023. Nara Park. All rights reserved.</p>
+        <div className="footer-bottom-right">
+          <p>Term of Services</p>
+          <p>Privacy Policy</p>
+          <p>Connect with me</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
