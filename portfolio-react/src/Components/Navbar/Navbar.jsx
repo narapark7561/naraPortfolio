cda47d1c71608cf224d84f95559cb1261033cdbb
narapark7561 <narapark7561@gmail.com>
2024-11-26 17:14:40 -0700

    Modified indentation

--- portfolio-react/src/Components/Navbar/Navbar.jsx
+++ portfolio-react/src/Components/Navbar/Navbar.jsx
@@ -9,20 +9,31 @@ const Navbar = () => {
     <div className='navbar'>
       <img src={nara} alt="nara's logo" />
       <ul className='nav-menu'>
-  <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>
-    <p>Home</p>
-  </li>
-  <li onClick={() => setMenu("about")} className={menu === "about" ? "active" : ""}>
-    <p>About Me</p>
-  </li>
-  <li onClick={() => setMenu("projects")} className={menu === "projects" ? "active" : ""}>
-    <p>Projects</p>
-  </li>
-  <li onClick={() => setMenu("contact")} className={menu === "contact" ? "active" : ""}>
-    <p>Contact</p>
-  </li>
-</ul>
-
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          <p>Home</p>
+        </li>
+        <li
+          onClick={() => setMenu("about")}
+          className={menu === "about" ? "active" : ""}
+        >
+          <p>About Me</p>
+        </li>
+        <li
+          onClick={() => setMenu("projects")}
+          className={menu === "projects" ? "active" : ""}
+        >
+          <p>Projects</p>
+        </li>
+        <li
+          onClick={() => setMenu("contact")}
+          className={menu === "contact" ? "active" : ""}
+        >
+          <p>Contact</p>
+        </li>
+      </ul>
       <div className='nav-connect'>Connect with Me</div>
     </div>
   )
