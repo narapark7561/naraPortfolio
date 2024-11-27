01e9825af7349cb7d3ec792f53fb48378d354e18
narapark7561 <narapark7561@gmail.com>
2024-11-26 17:12:39 -0700

    Added effect for menu bar

--- portfolio-react/src/Components/Navbar/Navbar.jsx
+++ portfolio-react/src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import nara from '../../assets/nara_final_logo.png'
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("home");
   return (
     <div className='navbar'>
-        <img src={nara} alt="nara's logo"/>
-        <ul className='nav-menu'>
-            <li>Home</li>
-            <li>About Me</li>
-            {/* <li>Experiences</li> */}
-            <li>Projects</li>
-            <li>Contact</li>
-        </ul>
-        <div className='nav-connect'>Connect with Me</div>
+      <img src={nara} alt="nara's logo" />
+      <ul className='nav-menu'>
+  <li onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>
+    <p>Home</p>
+  </li>
+  <li onClick={() => setMenu("about")} className={menu === "about" ? "active" : ""}>
+    <p>About Me</p>
+  </li>
+  <li onClick={() => setMenu("projects")} className={menu === "projects" ? "active" : ""}>
+    <p>Projects</p>
+  </li>
+  <li onClick={() => setMenu("contact")} className={menu === "contact" ? "active" : ""}>
+    <p>Contact</p>
+  </li>
+</ul>
+
+      <div className='nav-connect'>Connect with Me</div>
     </div>
   )
-}
+};  
 
 export default Navbar
